cf15e84cac155c56c0684df88ef7840e1af99308
salim <lesalgeriensdesstars@gmail.com>
2025-09-02 22:03:03 +0100

    moved scripts and make A MOVABLE LAMP

---
+++ static/scripts/script.js
@@ -0,0 +1,54 @@
+lamp = document.createElement("img")
+lamp.src="/static/images/lamp.png"
+
+lamp_hover = "w-7 absolute cursor-grab"
+lamp_grab =  "w-7 absolute cursor-grabbing"
+
+lamp.className=lamp_hover
+
+follower = document.createElement("div")
+follower.className="bg-yellow-100 absolute w-30 h-30 blur-2xl -z-10 rounded-full"
+
+follower.style.transform = "translate(-50%, -50%)";
+lamp.style.transform = "translate(-50%, -50%)";
+
+document.body.appendChild(follower)
+document.body.appendChild(lamp)
+
+dragElement(lamp)
+
+function dragElement(elmnt) {
+	let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+	elmnt.onmousedown = dragMouseDown;
+
+	function dragMouseDown(e) {
+		e = e || window.event;
+		e.preventDefault();
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+		elmnt.className=lamp_grab
+		document.onmouseup = closeDragElement;
+		document.onmousemove = elementDrag;
+	}
+
+	function elementDrag(e) {
+		e = e || window.event;
+		e.preventDefault();
+		pos1 = pos3 - e.clientX;
+		pos2 = pos4 - e.clientY;
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+		elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+		elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+
+		follower.style.top =  (elmnt.offsetTop - pos2) + "px";
+		follower.style.left = (elmnt.offsetLeft - pos1) + "px";
+
+	}
+
+	function closeDragElement() {
+		document.onmouseup = null;
+		document.onmousemove = null;
+		elmnt.className=lamp_hover
+	}
+}
