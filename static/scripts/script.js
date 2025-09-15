cfa1dc2780930ef85a417b03bac6a2c5ad35b8e2
salim <salim.belkacemm@protonmail.com>
2025-09-15 18:35:31 +0100

    added list of tools and made the elements individualy scrollable

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,54 +1,63 @@
-lamp = document.createElement("img")
-lamp.src="/static/images/lamp.png"
+function lamp(params) {
+	lamp = document.createElement("img")
+	lamp.src="/static/images/lamp.png"
 
-lamp_hover = "top-[70%] left-[80%] w-7 absolute cursor-grab"
-lamp_grab =  "top-[70%] left-[80%] w-7 absolute cursor-grabbing"
+	lamp_hover = "top-[70%] left-[80%] w-7 fixed cursor-grab"
+	lamp_grab =  "top-[70%] left-[80%] w-7 fixed cursor-grabbing"
 
-lamp.className=lamp_hover
+	lamp.className=lamp_hover
 
-follower = document.createElement("div")
-follower.className="top-[70%] left-[80%] bg-yellow-100 absolute w-30 h-30 blur-2xl -z-10 rounded-full"
+	follower = document.createElement("div")
+	follower.className="top-[70%] left-[80%] bg-yellow-100 fixed w-30 h-30 blur-2xl -z-10 rounded-full"
 
-follower.style.transform = "translate(-50%, -50%)";
-lamp.style.transform = "translate(-50%, -50%)";
+	follower.style.transform = "translate(-50%, -50%)";
+	lamp.style.transform = "translate(-50%, -50%)";
 
-document.body.appendChild(follower)
-document.body.appendChild(lamp)
+	document.body.appendChild(follower)
+	document.body.appendChild(lamp)
 
-dragElement(lamp)
+	dragElement(lamp)
 
-function dragElement(elmnt) {
-	let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-	elmnt.onmousedown = dragMouseDown;
+	function dragElement(elmnt) {
+		let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+		elmnt.onmousedown = dragMouseDown;
 
-	function dragMouseDown(e) {
-		e = e || window.event;
-		e.preventDefault();
-		pos3 = e.clientX;
-		pos4 = e.clientY;
-		elmnt.className=lamp_grab
-		document.onmouseup = closeDragElement;
-		document.onmousemove = elementDrag;
+		function dragMouseDown(e) {
+			e = e || window.event;
+			e.preventDefault();
+			pos3 = e.clientX;
+			pos4 = e.clientY;
+			elmnt.className=lamp_grab
+			document.onmouseup = closeDragElement;
+			document.onmousemove = elementDrag;
+		}
+
+		function elementDrag(e) {
+			e = e || window.event;
+			e.preventDefault();
+			pos1 = pos3 - e.clientX;
+			pos2 = pos4 - e.clientY;
+			pos3 = e.clientX;
+			pos4 = e.clientY;
+			elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+			elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+
+			follower.style.top =  (elmnt.offsetTop - pos2) + "px";
+			follower.style.left = (elmnt.offsetLeft - pos1) + "px";
+
+		}
+
+		function closeDragElement() {
+			document.onmouseup = null;
+			document.onmousemove = null;
+			elmnt.className=lamp_hover
+		}
 	}
 
-	function elementDrag(e) {
-		e = e || window.event;
-		e.preventDefault();
-		pos1 = pos3 - e.clientX;
-		pos2 = pos4 - e.clientY;
-		pos3 = e.clientX;
-		pos4 = e.clientY;
-		elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-		elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+}
 
-		follower.style.top =  (elmnt.offsetTop - pos2) + "px";
-		follower.style.left = (elmnt.offsetLeft - pos1) + "px";
+window.isDesktop = window.matchMedia("(min-width: 768px)").matches;
 
-	}
-
-	function closeDragElement() {
-		document.onmouseup = null;
-		document.onmousemove = null;
-		elmnt.className=lamp_hover
-	}
+if(window.isDesktop) {
+    lamp();
 }
